c493ac104a7b66379039fa503312b75053d34111
Michael <m.ding5692@gmail.com>
2015-08-06 22:44:13 -0400

    Create loginHandler.py

---
+++ userLogin/server/loginHandler.py
@@ -0,0 +1,15 @@
+# See https://github.com/contextio/Python-ContextIO for a guide
+
+import requests
+import contextio as c
+
+# contextio key and secret key
+CONSUMER_KEY = 'l57sr7jp'
+CONSUMER_SECRET = 'm0mRv5iaojsNWnvu'
+
+context_io = c.ContextIO(
+    consumer_key=CONSUMER_KEY, 
+    consumer_secret=CONSUMER_SECRET
+)
+
+requests.get
